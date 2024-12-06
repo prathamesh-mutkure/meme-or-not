e4cd49834159ca52a4041a59fc584915d24be383
Prakhar <prakharojha12@gmail.com>
2024-12-07 00:18:07 +0530

    Adding wagmi

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Providers } from "./providers";
+import '@coinbase/onchainkit/styles.css';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -2,13 +2,13 @@
  
 import type { ReactNode } from 'react';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
-import { base } from 'wagmi/chains'; // add baseSepolia for testing
+import { baseSepolia } from 'wagmi/chains'; // add baseSepolia for testing
  
 export function Providers(props: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-      chain={base} // add baseSepolia for testing
+      chain={baseSepolia} // add baseSepolia for testing
     >
       {props.children}
     </OnchainKitProvider>
