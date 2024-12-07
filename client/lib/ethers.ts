8553d176857142f893e7c2b4ac505bed19807c9a
Prakhar <prakharojha12@gmail.com>
2024-12-08 03:35:12 +0530

    Fixed Contract changes

--- client/lib/ethers.ts
+++ client/lib/ethers.ts
@@ -1,3 +1,3 @@
 import ABI from "../contractData/MemeOrNot.json";
-export const DEPLOYED_CONTRACT = '0xF91D4548a7E2a93fCC218ADC7ce619DCD53a24e3';
+export const DEPLOYED_CONTRACT = '0x0C4dcACda4eeedA552959EB71b5fAd708914fe60';
 export const CONTRACT_ABI = ABI.abi;
